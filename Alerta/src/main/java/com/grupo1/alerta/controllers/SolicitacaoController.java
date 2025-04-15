610c6e20a7846623fd9441987bb0ce888e59b744
RenataArdito <10417520@mackenzista.com.br>
2025-04-15 19:36:28 -0300

    Commit Pag Home

--- Alerta/src/main/java/com/grupo1/alerta/controllers/SolicitacaoController.java
+++ Alerta/src/main/java/com/grupo1/alerta/controllers/SolicitacaoController.java
@@ -6,7 +6,6 @@ import com.grupo1.alerta.services.SolicitacaoService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
